77dc42d9c4ee521e44cd89076ad07e9996a8930a
George Booth <george.booth@kcl.ac.uk>
2022-07-25 11:36:52 +0100

    Add orbital energies to static part of SE

--- example.py
+++ example.py
@@ -7,7 +7,7 @@ from moment_gw import AGW
 mol = gto.M(
         atom="Li 0 0 0; H 0 0 1.64",
         basis="cc-pvdz",
-        verbose=4,
+        verbose=5,
 )
 
 mf = dft.RKS(mol)
@@ -20,9 +20,9 @@ print(np.max(exact.mo_energy[mf.mo_occ > 0]))
 
 mf = mf.density_fit()
 
-for n in range(3):
-    gw = AGW(mf)
-    gw.diag_sigma = True
-    conv, gf, se = gw.kernel(nmom=2*n+1)
-    gf.remove_uncoupled(tol=1e-8)
-    print(gf.get_occupied().energy.max())
+#for n in range(3):
+gw = AGW(mf)
+gw.diag_sigma = True 
+conv, gf, se = gw.kernel(nmom=5, vhf_df=True)
+gf.remove_uncoupled(tol=1e-8)
+print(gf.get_occupied().energy)
