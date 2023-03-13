f310eadc7f2e5b794909ed8dd4b98581132d61d9
Ollie Backhouse <olbackhouse@gmail.com>
2023-03-13 21:41:06 +0000

    Adds __init__.py

---
+++ momentGW/__init__.py
@@ -0,0 +1,29 @@
+"""
+******************************
+momentGW: Moment-conserving GW
+******************************
+
+This repository contains the code and implementation for the paper
+"A 'moment-conesrving' reformulation of GW theory".
+
+Installation
+------------
+
+Install Vayesta, which includes other dependencies such as PySCF
+and NumPy:
+
+    git clone git@github.com:BoothGroup/Vayesta.git
+    python -m pip install . --user
+
+Install moment-conserving Dyson equation solver:
+
+    git clone git@github.com:BoothGroup/dyson.git
+
+Clone the momentGW repository:
+
+    git clone git@github.com:BoothGroup/momentGW.git --depth 1
+    python -m pip install . --user
+
+"""
+
+__version__ = "1.0.0"
