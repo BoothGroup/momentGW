3d40ac404cf93f1db763e9274af6bf14ecf7d300
Ollie Backhouse <olbackhouse@gmail.com>
2023-03-15 13:27:56 +0000

    Adds evGW

---
+++ examples/01-evgw.py
@@ -0,0 +1,25 @@
+"""Example of running evGW calculations.
+"""
+
+import numpy as np
+from pyscf import gto, dft
+from momentGW.evgw import evGW
+
+mol = gto.M(
+        atom="Li 0 0 0; H 0 0 1.64",
+        basis="cc-pvdz",
+        verbose=5,
+)
+
+mf = dft.RKS(mol)
+mf = mf.density_fit()
+mf.xc = "hf"
+mf.kernel()
+
+# evGW
+gw = evGW(mf)
+gw.kernel(nmom_max=3)
+
+# evGW_0
+gw = evGW(mf, w0=True)
+gw.kernel(nmom_max=3)
