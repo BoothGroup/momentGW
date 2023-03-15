91b27d446d2cb481905696c5d7244bc6f382e822
Ollie Backhouse <olbackhouse@gmail.com>
2023-03-15 10:05:38 +0000

    Adds simple example

---
+++ examples/00-g0w0.py
@@ -0,0 +1,20 @@
+"""Example of running a G0W0 calculation.
+"""
+
+import numpy as np
+from pyscf import gto, dft
+from momentGW.gw import GW
+
+mol = gto.M(
+        atom="Li 0 0 0; H 0 0 1.64",
+        basis="cc-pvdz",
+        verbose=5,
+)
+
+mf = dft.RKS(mol)
+mf = mf.density_fit()
+mf.xc = "hf"
+mf.kernel()
+
+gw = GW(mf)
+gw.kernel(nmom_max=3)
