db456f7967626459d5dca8fdecbabb086c75ea04
Ollie Backhouse <olbackhouse@gmail.com>
2023-08-01 16:55:58 +0100

    Adds KGW tests

---
+++ tests/test_kgw.py
@@ -0,0 +1,71 @@
+"""
+Tests for `kgw.py`
+"""
+
+import unittest
+
+import numpy as np
+import pytest
+from pyscf.pbc import gto, dft
+from pyscf.pbc.tools import k2gamma
+from pyscf.agf2 import mpi_helper
+
+from momentGW import GW, KGW
+
+
+class Test_KGW(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cell = gto.Cell()
+        cell.atom = "He 0 0 0; He 1 0 1"
+        cell.basis = "6-31g"
+        cell.a = np.eye(3) * 3
+        cell.precision = 1e-7
+        cell.verbose = 0
+        cell.build()
+
+        kmesh = [2, 2, 2]
+        kpts = cell.make_kpts(kmesh)
+
+        mf = dft.KRKS(cell, kpts, xc="hf")
+        mf = mf.density_fit(auxbasis="weigend")
+        mf.conv_tol = 1e-10
+        mf.kernel()
+
+        for k in range(len(kpts)):
+            mf.mo_coeff[k] = mpi_helper.bcast_dict(mf.mo_coeff[k], root=0)
+            mf.mo_energy[k] = mpi_helper.bcast_dict(mf.mo_energy[k], root=0)
+
+        smf = k2gamma.k2gamma(mf, kmesh=kmesh)
+        smf = smf.density_fit(auxbasis="weigend")
+
+        cls.cell, cls.kpts, cls.mf, cls.smf = cell, kpts, mf, smf
+
+    @classmethod
+    def tearDownClass(cls):
+        del cls.cell, cls.kpts, cls.mf, cls.smf
+
+    def test_supercell_valid(self):
+        # Require real MOs for supercell comparison
+        self.assertAlmostEqual(np.max(np.abs(np.array(self.mf.mo_coeff).imag)), 0, 8)
+
+    def test_dtda_vs_supercell(self):
+        nmom_max = 5
+
+        kgw = KGW(self.mf)
+        kgw.polarizability = "dtda"
+        kgw.kernel(nmom_max)
+
+        gw = GW(self.smf)
+        gw.polarizability = "dtda"
+        gw.kernel(nmom_max)
+
+        e1 = np.sort(np.concatenate([gf.energy for gf in kgw.gf]))
+        e2 = gw.gf.energy
+
+        np.testing.assert_allclose(e1, e2, atol=1e-8)
+
+
+if __name__ == "__main__":
+    print("Running tests for KGW")
+    unittest.main()
