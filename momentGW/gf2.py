f7cc10008a19c65b0b3c9780cc3ebe66b7760367
Oliver Backhouse <olbackhouse@gmail.com>
2024-03-28 11:33:02 +0000

    GF2 fixes

--- momentGW/gf2.py
+++ momentGW/gf2.py
@@ -32,19 +32,50 @@ class GF2:
         If `None`, use `gw.mo_occ` for both. Default value is `None`.
     """
 
-    def __init__(self, *args, non_dyson=True, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(
+        self,
+        gw,
+        nmom_max,
+        integrals,
+        mo_energy=None,
+        mo_occ=None,
+        non_dyson=False,
+    ):
+        self.gw = gw
+        self.nmom_max = nmom_max
+        self.integrals = integrals
         self.non_dyson = non_dyson
+
+        # Get the MO energies for G and W
+        if mo_energy is not None:
+            self.mo_energy_g = mo_energy["g"]
+            self.mo_energy_w = mo_energy["w"]
+        else:
+            self.mo_energy_g = self.mo_energy_w = gw.mo_energy
+
+        # Get the MO occupancies for G and W
+        if mo_occ is not None:
+            self.mo_occ_g = mo_occ["g"]
+            self.mo_occ_w = mo_occ["w"]
+        else:
+            self.mo_occ_g = self.mo_occ_w = gw.mo_occ
 
         # Check the MO energies and occupancies
         if not (
-            np.allclose(self.mo_energy["g"], self.mo_energy["w"])
-            and np.allclose(self.mo_occ["g"], self.mo_occ["w"])
+            np.allclose(self.mo_energy_g, self.mo_energy_w)
+            and np.allclose(self.mo_occ_g, self.mo_occ_w)
         ):
             raise ValueError(
                 "MO energies for Green's function and screened Coulomb "
                 f"interaction must be the same for {self.__class__.__name__}."
             )
+
+        # Options and thresholds
+        self.report_quadrature_error = True
+        if self.gw.compression and "ia" in self.gw.compression.split(","):
+            self.compression_tol = gw.compression_tol
+        else:
+            self.compression_tol = None
 
     def kernel(self):
         """
@@ -97,12 +128,16 @@ class GF2:
         e_i = self.mo_energy_g[self.mo_occ_g > 0]
         e_a = self.mo_energy_g[self.mo_occ_g == 0]
 
+        # Get the integrals
+        Lia = self.integrals.Lia.reshape(-1, e_i.size, e_a.size)
+        Lai = Lia.swapaxes(1, 2)
+
         # Get the occupied moments
         for i in range(*self.mpi_slice(e_i.size)):
-            pija = util.einsum("Pp,Pja->pja", self.integrals.Lpx[po, :, i], self.integrals.Lia)
-            pjia = util.einsum("Ppj,Pa->pja", self.integrals.Lpx[po], self.integrals.Lia[:, i])
+            pija = util.einsum("Pp,Pja->pja", self.integrals.Lpx[po, :, i], Lia)
+            pjia = util.einsum("Ppj,Pa->pja", self.integrals.Lpx[po, :, : e_i.size], Lia[:, i])
             pjia = 2 * pija - pjia
-            e_ija = e_i[i] + util.direct_sum("j,a->ja", e_i, -e_a)
+            e_ija = e_i[i] + e_i[:, None] - e_a[None, :]
 
             # Loop over orders
             for n in range(self.nmom_max + 1):
@@ -112,10 +147,10 @@ class GF2:
 
         # Get the virtual moments
         for a in range(*self.mpi_slice(e_a.size)):
-            pabi = util.einsum("Pp,Pbi->pbi", self.integrals.Lpx[pv, :, a], self.integrals.Lia)
-            pbai = util.einsum("Ppb,Pi->pbi", self.integrals.Lpx[pv], self.integrals.Lia[:, a])
+            pabi = util.einsum("Pp,Pbi->pbi", self.integrals.Lpx[pv, :, e_i.size + a], Lai)
+            pbai = util.einsum("Ppb,Pi->pbi", self.integrals.Lpx[pv, :, e_i.size :], Lai[:, a])
             pbai = 2 * pabi - pbai
-            e_abi = e_a[a] - util.direct_sum("b,i->bi", e_a, -e_i)
+            e_abi = e_a[a] + e_a[:, None] - e_i[None, :]
 
             # Loop over orders
             for n in range(self.nmom_max + 1):
@@ -127,3 +162,50 @@ class GF2:
         moments_vir = mpi_helper.allreduce(moments_vir)
 
         return moments_occ, moments_vir
+
+    @property
+    def nmo(self):
+        """Number of MOs."""  # TODO: do we want fuller documentation?
+        return self.gw.nmo
+
+    @property
+    def naux(self):
+        """Number of auxiliaries."""
+        return self.integrals.naux
+
+    def mpi_slice(self, n):
+        """
+        Return the start and end index for the current process for total
+        size `n`.
+
+        Parameters
+        ----------
+        n : int
+            Total size.
+
+        Returns
+        -------
+        p0 : int
+            Start index for current process.
+        p1 : int
+            End index for current process.
+        """
+        return list(mpi_helper.prange(0, n, n))[0]
+
+    def mpi_size(self, n):
+        """
+        Return the number of states in the current process for total size
+        `n`.
+
+        Parameters
+        ----------
+        n : int
+            Total size.
+
+        Returns
+        -------
+        size : int
+            Number of states in current process.
+        """
+        p0, p1 = self.mpi_slice(n)
+        return p1 - p0
