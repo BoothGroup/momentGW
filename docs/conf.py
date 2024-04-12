fe1f7266c78b70c29b869f7e3a215e9261ad1581
Oliver Backhouse <olbackhouse@gmail.com>
2024-04-12 21:31:04 +0100

    Customise conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,27 +1,19 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# docs/conf.py
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../"))
 
-project = 'momentGW'
-copyright = '2024, Oliver J. Backhouse'
-author = 'Oliver J. Backhouse'
+project = "momentGW"
+copyright = "2024, Oliver J. Backhouse"
+author = "Oliver J. Backhouse"
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+source_suffix = [".rst", ".md"]
+master_doc = "index"
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
